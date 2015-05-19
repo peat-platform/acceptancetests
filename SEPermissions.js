5ec716ae233ff38c0a702e0ddbe98aaa422b181a
dmccarthy <dmccarthy@tssg.org>
2015-05-19 13:05:49 +0100

    Added tests for object creation

--- SEPermissions.js
+++ SEPermissions.js
@@ -4,7 +4,7 @@
 'use strict';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var supertest = require('supertest-as-promised');
-var request = supertest('https://dev.openi-ict.eu');
+var request = supertest('https://demo2.openi-ict.eu');
 var assert = require('chai').assert;
 
 

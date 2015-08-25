00966a306c66c033b23eaf67a81c6b7d647ccd82
Dylan Conway <dconway@tssg.org>
2015-08-25 15:58:41 +0000

    testing jenkins tests

--- config.js
+++ config.js
@@ -5,8 +5,12 @@
 /** This config allows jenkins to run tests.
  *  Change these values to point to your dev environment but DO NOT COMMIT this file **/
 
-module.exports.url      = "https://peat.wizeoni.com";
-module.exports.internal = "https://peat.wizeoni.com:8443";
+//module.exports.url      = "https://peat.wizeoni.com";
+//module.exports.internal = "https://peat.wizeoni.com:8443";
+
+module.exports.url      = "https://10.37.83.4";
+module.exports.internal = "https://10.37.83.4:8443";
+
 
 module.exports.devurl      = "https://dev.peat-platform.org";
 module.exports.devinternal = "https://dev.peat-platform.org:8443";
